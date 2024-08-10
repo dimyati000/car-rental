078a30820e564103dea30c360c8d9608833c6d73
dimyati000 <dimyatiazhar000@gmail.com>
2024-08-10 10:55:43 +0700

    cek

--- application/views/system/formSewa/penumpang.php
+++ application/views/system/formSewa/penumpang.php
@@ -239,7 +239,7 @@
 <!-- <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script> -->
 <script src="<?php echo base_url(); ?>assets/modules/select2/dist/js/select2.full.min.js"></script>
-<script>
+<script> 
 $(".select2").select2();
 $('#jamBerangkat').datetimepicker({
     "allowInputToggle": true,
